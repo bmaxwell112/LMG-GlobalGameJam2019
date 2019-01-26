1d4fbc07397f1c275ca645645269f70d24077aca
Cody <lordxiek@gmail.com>
2019-01-26 13:37:34 -0500

    More question stuff

--- Assets/Controllers/QuestionController.cs
+++ Assets/Controllers/QuestionController.cs
@@ -4,41 +4,89 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class QuestionController : MonoBehaviour {
-
-public bool[] questions;
+bool[] questions = {People, Fire, Spaces, Heights};
 public static bool People, Fire, Spaces, Heights;
 private bool currentBoolean;
+private int arrayValue;
 private Image QuestionImage;
 public Sprite image1, image2, image3, image4;
 
 	// Use this for initialization
 	void Start () {
-		bool[] questions = new bool[]{People, Fire, Spaces, Heights};
 
+		arrayValue = 0;
 		QuestionImage = FindObjectOfType<currentimage>().GetComponent<Image>();
-		QuestionImage.sprite = image1;
 		SetCurrentBoolean(questions[0]);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		SetCurrentQuestion();
 	}
 
 	void SetCurrentBoolean(bool question)
 	{
-		currentBoolean = question;
+		question = currentBoolean;
+	}
+
+	void SetCurrentQuestion()
+	{
+
+		switch (arrayValue)
+		{
+			case 0:
+			QuestionImage.sprite = image1;
+			break;
+
+			case 1:
+			QuestionImage.sprite = image2;
+			break;
+			
+			case 2:
+			QuestionImage.sprite = image3;		
+			break;
+
+			case 3:
+			QuestionImage.sprite = image4;
+			break;
+
+			default:
+			print("No question");
+			break;
+		}
 	}
 
 	public void Good()
 	{
 		currentBoolean = true;
-		
+		if(arrayValue < 3)
+		{
+		arrayValue++;
+		SetCurrentBoolean(questions[arrayValue]);
+		}
+		else
+		{
+			NextScene();
+		}
+
 	}
 
 	public void Bad()
 	{
 		currentBoolean = false;
-		//increment to next boolean in array
+		if(arrayValue < 3)
+		{
+		arrayValue++;
+		SetCurrentBoolean(questions[arrayValue]);
+		}
+		else
+		{
+			NextScene();
+		}
+	}
+
+	void NextScene()
+	{
+		print(questions[0]);
 	}
 }
