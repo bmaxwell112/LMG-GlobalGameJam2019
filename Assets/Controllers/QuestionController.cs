ecf707c1d93e8d45da05462c8c12de1f5d167035
Cody <lordxiek@gmail.com>
2019-01-26 11:34:18 -0500

    Started object generation

---
+++ Assets/Controllers/QuestionController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestionController : MonoBehaviour {
+
+public static bool People, Fire, Spaces, Heights;
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Environment/ToggleFire.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToggleFire : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		ChooseModel();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void ChooseModel()
+	{
+		if(!QuestionController.Fire)
+		{
+			//Fire Particles object, remove when hiding
+		}
+	}
+}
