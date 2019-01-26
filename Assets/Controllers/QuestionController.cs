8c06fb79f058b5bfe9c5e429767ccc47cdb460dc
Cody <lordxiek@gmail.com>
2019-01-26 13:11:10 -0500

    Questions UI

--- Assets/Controllers/QuestionController.cs
+++ Assets/Controllers/QuestionController.cs
@@ -1,19 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class QuestionController : MonoBehaviour {
 
+public bool[] questions;
 public static bool People, Fire, Spaces, Heights;
-
+private bool currentBoolean;
+private Image QuestionImage;
+public Sprite image1, image2, image3, image4;
 
 	// Use this for initialization
 	void Start () {
+		bool[] questions = new bool[]{People, Fire, Spaces, Heights};
 
+		QuestionImage = FindObjectOfType<currentimage>().GetComponent<Image>();
+		QuestionImage.sprite = image1;
+		SetCurrentBoolean(questions[0]);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void SetCurrentBoolean(bool question)
+	{
+		currentBoolean = question;
+	}
+
+	public void Good()
+	{
+		currentBoolean = true;
+		
+	}
+
+	public void Bad()
+	{
+		currentBoolean = false;
+		//increment to next boolean in array
 	}
 }
--- Assets/Environment/ToggleSpaces.cs
+++ Assets/Environment/ToggleSpaces.cs
@@ -6,11 +6,23 @@ public class ToggleSpaces : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		
+		ChooseModel();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	void ChooseModel()
+	{
+		if(QuestionController.Spaces)
+		{
+			//No walls until hiding
+		}
+		else
+		{
+			//Generate more objects, closer walls, remove walls when hiding
+		}
 	}
 }
