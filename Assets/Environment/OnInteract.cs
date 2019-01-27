b114e4258ff0b28b98d21884bf75ef343c8b547f
Brett Maxwell <bmaxwell112@gmail.com>
2019-01-27 14:26:44 -0500

    Final Changes

--- Assets/Environment/OnInteract.cs
+++ Assets/Environment/OnInteract.cs
@@ -7,7 +7,7 @@ public class OnInteract : MonoBehaviour {
 		enum Interactions { button, collect, door }
 
 		[SerializeField] Interactions interactions;
-
+		[SerializeField] Animator anim = null;
 	// Use this for initialization
 	void Start () {
 		
@@ -36,6 +36,8 @@ public class OnInteract : MonoBehaviour {
 			if(TrackHearts.hearts == 2)
 			{
 				//open door
+				anim.SetBool("Open", true);
+				Destroy(gameObject);
 			}
 		}
 	}
