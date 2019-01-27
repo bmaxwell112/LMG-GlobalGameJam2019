1b8a97a7927862605150a1fe1092fd4cf238dd07
Cody <lordxiek@gmail.com>
2019-01-27 12:55:41 -0500

    Finished hearts

--- Assets/managers/TrackHearts.cs
+++ Assets/managers/TrackHearts.cs
@@ -12,20 +12,21 @@ public static int hearts;
 	// Use this for initialization
 	void Start () {
 		hearts = 0;
-		// FindObjectOfType<heart1>().GetComponent<Renderer>().enabled = false;
-		// FindObjectOfType<heart2>().GetComponent<Renderer>().enabled = false;
+		print(hearts);
+		FindObjectOfType<heart1>().GetComponent<Image>().enabled = false;
+		FindObjectOfType<heart2>().GetComponent<Image>().enabled = false;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		if(hearts == 1)
 		{
-			// FindObjectOfType<heart1>().GetComponent<Renderer>().enabled = true;
+			FindObjectOfType<heart1>().GetComponent<Image>().enabled = true;
 		}
 
 		if(hearts == 2)
 		{
-			// FindObjectOfType<heart2>().GetComponent<Renderer>().enabled = true;
+			FindObjectOfType<heart2>().GetComponent<Image>().enabled = true;
 		}
 	}
 }
