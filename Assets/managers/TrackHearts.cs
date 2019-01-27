b22b97245b8644f49f3889d17fff6656a7457e98
Cody <lordxiek@gmail.com>
2019-01-26 21:41:04 -0500

    Added hearts, interaction types, basic interactable item

---
+++ Assets/managers/TrackHearts.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrackHearts : MonoBehaviour {
+
+public static int hearts;
+
+	// Use this for initialization
+	void Start () {
+		hearts = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//replace this with heart images
+		Text heartCount = GetComponentInChildren<Text>();
+		heartCount.text = hearts.ToString();
+	}
+}
